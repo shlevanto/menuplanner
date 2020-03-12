0cfe41ce91c134f61b8af2d96623eb47a92e2998
shlevanto <simo.levanto@helsinki.fi>
2020-03-12 08:58:47 +0200

    jdbc -tietokantayhteyden konfigurointi ja testaaminen

--- Menuplanner/src/main/java/ohte/menuplanner/Main.java
+++ Menuplanner/src/main/java/ohte/menuplanner/Main.java
@@ -9,6 +9,7 @@ package ohte.menuplanner;
  *
  * @author levantsi
  */
+import java.sql.*;
 import javafx.application.Application;
 
 import javafx.stage.Stage;
@@ -21,9 +22,24 @@ import javafx.scene.layout.BorderPane;
  * @author levantsi
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello sunshine!");
         GraphicUI.main(args);
+        
+        
+        Connection db = DriverManager.getConnection("jdbc:sqlite:testi.db");
+        Statement s = db.createStatement();
+        s.execute("CREATE TABLE Tuotteet (id INTEGER PRIMARY KEY, nimi TEXT, hinta INTEGER)");
+        s.execute("INSERT INTO Tuotteet (nimi,hinta) VALUES ('retiisi',7)");
+        s.execute("INSERT INTO Tuotteet (nimi,hinta) VALUES ('porkkana',5)");
+        s.execute("INSERT INTO Tuotteet (nimi,hinta) VALUES ('nauris',4)");
+        s.execute("INSERT INTO Tuotteet (nimi,hinta) VALUES ('lanttu',8)");
+        s.execute("INSERT INTO Tuotteet (nimi,hinta) VALUES ('selleri',4)");
+
+        ResultSet r = s.executeQuery("SELECT * FROM Tuotteet");
+        while (r.next()) {
+            System.out.println(r.getInt("id")+" "+r.getString("nimi")+" "+r.getInt("hinta"));
+        }
     }
     
 }
--- dokumentaatio/tyoaika.md
+++ dokumentaatio/tyoaika.md
@@ -2,3 +2,4 @@ pv | aika | kuvaus
 -- | ---- | ------
 12.3. | 1,5 | vaatimusmäärittelyn kirjoittaminen, linkitykset
 12.3. | 0,5 | harjoitustyöprojektin rakentaminen repositorioon
+13.3. | 0,5 | tietokantayhteyden konfigurointi ja testaaminen Maven
